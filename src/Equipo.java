8dda8af85dee0f7933ec076ad10612a88959c14a
MiaElvir <melvirrojas@gmail.com>
2024-02-23 14:40:26 -0600

    commit inicial

---
+++ src/Equipo.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Usuario
+ */
+public class Equipo {
+    
+    protected String pais, nombre, ciudad, estadio; 
+
+    public Equipo(String pais, String nombre, String ciudad, String estadio) {
+        this.pais = pais;
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.estadio = estadio;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" + "pais=" + pais + ", nombre=" + nombre + ", ciudad=" + ciudad + ", estadio=" + estadio + '}';
+    }
+    
+    
+    
+    
+}
---
+++ src/Jugador.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Jugador {
+    
+    protected String nombre, posicion; 
+    protected int edad; 
+
+    public Jugador(String nombre, String posicion, int edad) {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "nombre=" + nombre + ", posicion=" + posicion + ", edad=" + edad + '}';
+    }
+    
+    
+}
